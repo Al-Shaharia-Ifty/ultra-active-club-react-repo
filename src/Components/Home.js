ec67f2545479f237d313fedf984d1c3290e9c183
Al Shaharia <ifty682002@gmail.com>
2022-09-29 21:01:14 +0600

    question and answer is done.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -41,21 +41,24 @@ const Home = ({ timer, setTimer }) => {
             What is difference between props and state?
           </h2>
           <p className="text-xl">
-            ReactJS divides the UI into isolated reusable pieces of code known
-            as components. React components work similarly to JavaScript
-            functions as they accept arbitrary inputs called properties or
-            props. It's possible to have as many components as necessary without
-            cluttering your code.
+            State is the local state of the component which cannot be accessed
+            and modified outside of the component. It's equivalent to local
+            variables in a function. Props, on the other hand, make components
+            reusable by giving components the ability to receive data from their
+            parent component in the form of props.
           </p>
         </div>
         <div className="mt-5">
-          <h2 className="text-3xl mb-5">Who does React Work?</h2>
+          <h2 className="text-3xl mb-5">
+            What does useEffect do without data loading?
+          </h2>
           <p className="text-xl">
-            ReactJS divides the UI into isolated reusable pieces of code known
-            as components. React components work similarly to JavaScript
-            functions as they accept arbitrary inputs called properties or
-            props. It's possible to have as many components as necessary without
-            cluttering your code.
+            By using this Hook, you tell React that your component needs to do
+            something after render. React will remember the function you passed
+            we'll refer to it as our “effect”, and call it later after
+            performing the DOM updates. In this effect, we set the document
+            title, but we could also perform data fetching or call some other
+            imperative API.
           </p>
         </div>
       </div>
