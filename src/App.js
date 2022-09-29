4f2d91ac90312cc4bfc5ccd301392c90a95682e0
Al Shaharia <ifty682002@gmail.com>
2022-09-29 12:27:32 +0600

    home and cart page add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("FakeData.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, [setData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-3 gap-4">
+      <div className="col-span-2">
+        <Home />
+      </div>
+      <div>
+        <Cart />
+      </div>
     </div>
   );
 }
